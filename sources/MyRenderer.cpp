265caa013005f95a70a851359cb90799e3aaf068
worker-Windows <yangpan0822@qq.com>
2024-07-18 20:07:18 +0800

    创建渲染管线

--- sources/MyRenderer.cpp
+++ sources/MyRenderer.cpp
@@ -1,5 +1,25 @@
 #include "MyRenderer.h"
 #include <QDebug>
+#include <QVulkanDeviceFunctions>
+#include <fstream>
+
+namespace {
+std::vector<char> ReadFile(const std::string& fileName)
+{
+    std::ifstream file(fileName, std::ios::ate | std::ios::binary);
+    if (!file.is_open())
+    {
+        throw std::runtime_error("failed to open file: " + fileName);
+    }
+
+    size_t fileSize = static_cast<size_t>(file.tellg());
+    std::vector<char> buffer(fileSize);
+    file.seekg(0);
+    file.read(buffer.data(), fileSize);
+    file.close();
+    return buffer;
+}
+} // namespace
 
 MyRenderer::MyRenderer(QVulkanWindow* window)
     : m_window(window)
@@ -9,5 +29,182 @@ MyRenderer::MyRenderer(QVulkanWindow* window)
 
 void MyRenderer::startNextFrame()
 {
+    qDebug() << "startNextFrame";
+
     m_window->frameReady();
+}
+
+void MyRenderer::initResources()
+{
+    qDebug() << "initResources";
+
+    m_device   = m_window->device();
+    m_devFuncs = m_window->vulkanInstance()->deviceFunctions(m_device);
+
+    auto vertShaderCode = ReadFile(SHADER_DIR + std::string("/triangle_vert.spv"));
+    auto fragShaderCode = ReadFile(SHADER_DIR + std::string("/triangle_frag.spv"));
+
+    VkShaderModule vertShaderModule = CreateShaderModule(vertShaderCode);
+    VkShaderModule fragShaderModule = CreateShaderModule(fragShaderCode);
+
+    VkPipelineShaderStageCreateInfo vertShaderStageInfo = {
+        .sType               = VK_STRUCTURE_TYPE_PIPELINE_SHADER_STAGE_CREATE_INFO,
+        .stage               = VK_SHADER_STAGE_VERTEX_BIT,
+        .module              = vertShaderModule,
+        .pName               = "main",
+        .pSpecializationInfo = nullptr
+    };
+
+    VkPipelineShaderStageCreateInfo fragShaderStageInfo = {
+        .sType               = VK_STRUCTURE_TYPE_PIPELINE_SHADER_STAGE_CREATE_INFO,
+        .stage               = VK_SHADER_STAGE_FRAGMENT_BIT,
+        .module              = fragShaderModule,
+        .pName               = "main",
+        .pSpecializationInfo = nullptr
+    };
+
+    VkPipelineShaderStageCreateInfo shaderStages[] = {vertShaderStageInfo, fragShaderStageInfo};
+
+    // 顶点信息
+    VkPipelineVertexInputStateCreateInfo vertexInputInfo = {
+        .sType                           = VK_STRUCTURE_TYPE_PIPELINE_VERTEX_INPUT_STATE_CREATE_INFO,
+        .vertexBindingDescriptionCount   = 0,
+        .pVertexBindingDescriptions      = nullptr,
+        .vertexAttributeDescriptionCount = 0,
+        .pVertexAttributeDescriptions    = nullptr
+    };
+
+    // 拓扑信息
+    VkPipelineInputAssemblyStateCreateInfo inputAssembly = {
+        .sType                  = VK_STRUCTURE_TYPE_PIPELINE_INPUT_ASSEMBLY_STATE_CREATE_INFO,
+        .topology               = VK_PRIMITIVE_TOPOLOGY_TRIANGLE_LIST,
+        .primitiveRestartEnable = VK_FALSE
+    };
+
+    // 视口、裁剪
+    VkPipelineViewportStateCreateInfo viewportState = {
+        .sType = VK_STRUCTURE_TYPE_PIPELINE_VIEWPORT_STATE_CREATE_INFO, .viewportCount = 1, .scissorCount = 1
+    };
+
+    // 光栅化
+    VkPipelineRasterizationStateCreateInfo rasterizer = {
+        .sType                   = VK_STRUCTURE_TYPE_PIPELINE_RASTERIZATION_STATE_CREATE_INFO,
+        .depthClampEnable        = VK_FALSE,
+        .rasterizerDiscardEnable = VK_FALSE,
+        .polygonMode             = VK_POLYGON_MODE_FILL,
+        .cullMode                = VK_CULL_MODE_BACK_BIT,
+        .frontFace               = VK_FRONT_FACE_CLOCKWISE,
+        .depthBiasEnable         = VK_FALSE,
+        .lineWidth               = 1.f
+    };
+
+    // 多重采样
+    VkPipelineMultisampleStateCreateInfo multisampling = {
+        .sType                 = VK_STRUCTURE_TYPE_PIPELINE_MULTISAMPLE_STATE_CREATE_INFO,
+        .rasterizationSamples  = VK_SAMPLE_COUNT_1_BIT,
+        .sampleShadingEnable   = VK_FALSE,
+        .minSampleShading      = 1.f,
+        .pSampleMask           = nullptr,
+        .alphaToCoverageEnable = VK_FALSE,
+        .alphaToOneEnable      = VK_FALSE
+    };
+
+    // 深度模板测试
+    VkPipelineDepthStencilStateCreateInfo depthStencil = {
+        .sType                 = VK_STRUCTURE_TYPE_PIPELINE_DEPTH_STENCIL_STATE_CREATE_INFO,
+        .depthTestEnable       = VK_TRUE,
+        .depthWriteEnable      = VK_TRUE,
+        .depthCompareOp        = VK_COMPARE_OP_LESS,
+        .depthBoundsTestEnable = VK_FALSE,
+        .stencilTestEnable     = VK_FALSE,
+        .front                 = {},
+        .back                  = {},
+        .minDepthBounds        = 0.f,
+        .maxDepthBounds        = 1.f
+    };
+
+    // 颜色混合
+    VkPipelineColorBlendAttachmentState colorBlendAttachment {.blendEnable = VK_FALSE, .colorWriteMask = 0xF};
+    VkPipelineColorBlendStateCreateInfo colorBlending {
+        .sType           = VK_STRUCTURE_TYPE_PIPELINE_COLOR_BLEND_STATE_CREATE_INFO,
+        .attachmentCount = 1,
+        .pAttachments    = &colorBlendAttachment
+    };
+
+    // 动态状态
+    std::vector<VkDynamicState> dynamicStates = {VK_DYNAMIC_STATE_VIEWPORT, VK_DYNAMIC_STATE_SCISSOR};
+    VkPipelineDynamicStateCreateInfo dynamicState {
+        .sType             = VK_STRUCTURE_TYPE_PIPELINE_DYNAMIC_STATE_CREATE_INFO,
+        .dynamicStateCount = static_cast<uint32_t>(dynamicStates.size()),
+        .pDynamicStates    = dynamicStates.data()
+    };
+
+    // 管线布局
+    VkPipelineLayoutCreateInfo pipelineLayoutInfo {
+        .sType = VK_STRUCTURE_TYPE_PIPELINE_LAYOUT_CREATE_INFO, .setLayoutCount = 0, .pushConstantRangeCount = 0
+    };
+
+    if (VK_SUCCESS != m_devFuncs->vkCreatePipelineLayout(m_device, &pipelineLayoutInfo, nullptr, &m_pipelineLayout))
+    {
+        throw std::runtime_error("failed to create pipeline layout");
+    }
+
+    VkGraphicsPipelineCreateInfo pipelineInfo = {
+        .sType               = VK_STRUCTURE_TYPE_GRAPHICS_PIPELINE_CREATE_INFO,
+        .stageCount          = 2,
+        .pStages             = shaderStages,
+        .pVertexInputState   = &vertexInputInfo,
+        .pInputAssemblyState = &inputAssembly,
+        .pViewportState      = &viewportState,
+        .pRasterizationState = &rasterizer,
+        .pMultisampleState   = &multisampling,
+        .pDepthStencilState  = &depthStencil,
+        .pColorBlendState    = &colorBlending,
+        .pDynamicState       = &dynamicState,
+        .layout              = m_pipelineLayout,
+        .renderPass          = m_window->defaultRenderPass(),
+        .subpass             = 0,
+        .basePipelineHandle  = nullptr
+    };
+
+    if (VK_SUCCESS != m_devFuncs->vkCreateGraphicsPipelines(m_device, nullptr, 1, &pipelineInfo, nullptr, &m_pipeline))
+    {
+        throw std::runtime_error("failed to create pipeline");
+    }
+
+    if (vertShaderModule)
+    {
+        m_devFuncs->vkDestroyShaderModule(m_device, vertShaderModule, nullptr);
+    }
+    if (fragShaderModule)
+    {
+        m_devFuncs->vkDestroyShaderModule(m_device, fragShaderModule, nullptr);
+    }
+}
+
+void MyRenderer::releaseResources()
+{
+    qDebug() << "releaseResources";
+
+    if (m_pipeline)
+    {
+        m_devFuncs->vkDestroyPipeline(m_device, m_pipeline, nullptr);
+        m_pipeline = VK_NULL_HANDLE;
+    }
+}
+
+VkShaderModule MyRenderer::CreateShaderModule(const std::vector<char>& code) const
+{
+    VkShaderModuleCreateInfo createInfo = {};
+    createInfo.sType                    = VK_STRUCTURE_TYPE_SHADER_MODULE_CREATE_INFO;
+    createInfo.codeSize                 = code.size();
+    createInfo.pCode                    = reinterpret_cast<const uint32_t*>(code.data());
+
+    VkShaderModule shaderModule;
+    if (VK_SUCCESS != m_devFuncs->vkCreateShaderModule(m_device, &createInfo, nullptr, &shaderModule))
+    {
+        throw std::runtime_error("failed to create shader module");
+    }
+
+    return shaderModule;
 }
--- sources/MyRenderer.h
+++ sources/MyRenderer.h
@@ -1,14 +1,26 @@
 #pragma once
 
 #include <QVulkanWindow>
+#include <vector>
 
 class MyRenderer : public QVulkanWindowRenderer
 {
 public:
     MyRenderer(QVulkanWindow* window);
 
+    void initResources() override;
+    void releaseResources() override;
+
     void startNextFrame() override;
 
 private:
+    VkShaderModule CreateShaderModule(const std::vector<char>& code) const;
+
+private:
     QVulkanWindow* m_window {nullptr};
+    QVulkanDeviceFunctions* m_devFuncs {nullptr};
+
+    VkDevice m_device {VK_NULL_HANDLE};
+    VkPipelineLayout m_pipelineLayout {VK_NULL_HANDLE};
+    VkPipeline m_pipeline {VK_NULL_HANDLE};
 };
