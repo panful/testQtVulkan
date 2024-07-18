fda58bcdaa4c0ec9a684c89c8f0236c25d62fe67
worker-Windows <yangpan0822@qq.com>
2024-07-18 20:34:51 +0800

    开启Vulkan验证层

--- sources/main.cpp
+++ sources/main.cpp
@@ -1,13 +1,17 @@
 #include "MyWindow.h"
 #include <QDebug>
 #include <QGuiApplication>
+#include <QLoggingCategory>
 #include <QVulkanInstance>
 
 int main(int argc, char** argv)
 {
     QGuiApplication app(argc, argv);
 
+    QLoggingCategory::setFilterRules(QStringLiteral("qt.vulkan=true"));
+
     QVulkanInstance instance {};
+    instance.setLayers({"VK_LAYER_KHRONOS_validation"});
     if (!instance.create())
     {
         qDebug() << "failed to create vulkan instance: " << instance.errorCode();
