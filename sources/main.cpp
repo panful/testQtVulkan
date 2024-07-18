f35369c5dc750869107928eb995ebf083fa9b623
worker-Windows <yangpan0822@qq.com>
2024-07-18 17:21:17 +0800

    创建一个QtVulkan窗口

--- sources/main.cpp
+++ sources/main.cpp
@@ -1,10 +1,23 @@
 #include <QDebug>
 #include <QGuiApplication>
+#include <QVulkanInstance>
+#include <QVulkanWindow>
 #include <QWidget>
 
 int main(int argc, char** argv)
 {
     QGuiApplication app(argc, argv);
+
+    QVulkanInstance instance {};
+    if (!instance.create())
+    {
+        qDebug() << "failed to create vulkan instance: " << instance.errorCode();
+    }
+
+    QVulkanWindow window {};
+    window.setVulkanInstance(&instance);
+    window.resize(800, 600);
+    window.show();
 
     qDebug() << "test qt vulkan\n";
 
