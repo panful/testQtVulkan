606fd40285ea6e36fbaa6f10a42e31e194269663
worker-Windows <yangpan0822@qq.com>
2024-07-19 09:01:16 +0800

    添加事件处理

--- sources/MyWindow.cpp
+++ sources/MyWindow.cpp
@@ -1,6 +1,12 @@
 #include "MyWindow.h"
 #include "MyRenderer.h"
+#include <QCloseEvent>
 #include <QDebug>
+#include <QKeyEvent>
+#include <QMouseEvent>
+#include <QPaintEvent>
+#include <QResizeEvent>
+#include <QWheelEvent>
 
 MyWindow::MyWindow(QWindow* parent)
     : QVulkanWindow(parent)
@@ -10,5 +16,67 @@ MyWindow::MyWindow(QWindow* parent)
 
 QVulkanWindowRenderer* MyWindow::createRenderer()
 {
-    return new MyRenderer(this);
+    m_renderer = new MyRenderer(this);
+    return m_renderer;
+}
+
+void MyWindow::resizeEvent(QResizeEvent* event)
+{
+    qDebug() << event->type();
+    return QVulkanWindow::resizeEvent(event);
+}
+
+void MyWindow::paintEvent(QPaintEvent* event)
+{
+
+    qDebug() << event->type();
+    return QVulkanWindow::paintEvent(event);
+}
+
+void MyWindow::closeEvent(QCloseEvent* event)
+{
+    qDebug() << event->type();
+    return QVulkanWindow::closeEvent(event);
+}
+
+void MyWindow::keyPressEvent(QKeyEvent* event)
+{
+    qDebug() << event->type();
+    return QVulkanWindow::keyPressEvent(event);
+}
+
+void MyWindow::keyReleaseEvent(QKeyEvent* event)
+{
+    qDebug() << event->type();
+    return QVulkanWindow::keyReleaseEvent(event);
+}
+
+void MyWindow::mousePressEvent(QMouseEvent* event)
+{
+    qDebug() << event->type();
+    return QVulkanWindow::mousePressEvent(event);
+}
+
+void MyWindow::mouseReleaseEvent(QMouseEvent* event)
+{
+    qDebug() << event->type();
+    return QVulkanWindow::mouseReleaseEvent(event);
+}
+
+void MyWindow::mouseDoubleClickEvent(QMouseEvent* event)
+{
+    qDebug() << event->type();
+    return QVulkanWindow::mouseDoubleClickEvent(event);
+}
+
+void MyWindow::mouseMoveEvent(QMouseEvent* event)
+{
+    qDebug() << event->type();
+    return QVulkanWindow::mouseMoveEvent(event);
+}
+
+void MyWindow::wheelEvent(QWheelEvent* event)
+{
+    qDebug() << event->type();
+    return QVulkanWindow::wheelEvent(event);
 }
--- sources/MyWindow.h
+++ sources/MyWindow.h
@@ -8,4 +8,19 @@ public:
     explicit MyWindow(QWindow* parent = nullptr);
 
     QVulkanWindowRenderer* createRenderer() override;
+
+protected:
+    void resizeEvent(QResizeEvent* event) override;
+    void paintEvent(QPaintEvent* event) override;
+    void closeEvent(QCloseEvent* event) override;
+    void keyPressEvent(QKeyEvent* event) override;
+    void keyReleaseEvent(QKeyEvent* event) override;
+    void mousePressEvent(QMouseEvent* event) override;
+    void mouseReleaseEvent(QMouseEvent* event) override;
+    void mouseDoubleClickEvent(QMouseEvent* event) override;
+    void mouseMoveEvent(QMouseEvent* event) override;
+    void wheelEvent(QWheelEvent* event) override;
+
+private:
+    QVulkanWindowRenderer* m_renderer {nullptr};
 };
